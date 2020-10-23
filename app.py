7278836858b92e78202a1731c11ef7343b17ef77
skar94376 <skar94376@gmail.com>
2020-10-23 17:55:03 +0530

    indentation change in app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 @app.route('/get_location_names',methods=['GET'])
 def get_location_names():
     response = jsonify({
-        'locations': util.get_location_names()
-    })
+        'locations': util.get_location_names()})
     # response.headers.add('Access-Control-Allow-Origin', '*')
     # return response
     return render_template('index.html')
